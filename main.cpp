8ea6fa7c84cf989c16a30809e127881d39020f4f
Gwynard C. Andaya <87253742+PengGwyn@users.noreply.github.com>
2024-11-20 11:04:00 +0800

    add new stuffs

--- main.cpp
+++ main.cpp
@@ -1,37 +1,51 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
     int option = 0;
     float num1 = 0, num2 = 0, result = 0;
+    string willContinue;
+    bool willContinueB = false;
 
-    cout << "Enter the first number: ";
-    cin >> num1;
-    cout << "Enter the second number: ";
-    cin >> num2;
-    cout << "Choose operation: \n[1] addition, \n[2] subtraction, \n[3] multiplication, \n[4] division: ";
-    cin >> option;
+    do {
+        cout << "Enter the first number: ";
+        cin >> num1;
+        cout << "Enter the second number: ";
+        cin >> num2;
+        cout << "Choose operation: \n[1] addition, \n[2] subtraction, \n[3] multiplication, \n[4] division: ";
+        cin >> option;
 
-    if (option == 1) // addition
-    {
-        result = num1 + num2;
-    }
-    else if (option == 2)
-    {
-        result = num1 - num2;
-    }
-    else if (option == 3)
-    {
-        result = num1 * num2;
-    }
-    else if (option == 4)
-    {
-        result = num1 / num2;
-    }
+        if (option == 1) // addition
+        {
+            result = num1 + num2;
+        }
+        else if (option == 2)
+        {
+            result = num1 - num2;
+        }
+        else if (option == 3)
+        {
+            result = num1 * num2;
+        }
+        else if (option == 4)
+        {
+            result = num1 / num2;
+        }
 
-    cout << "Result is: " << result << endl;
-
+        cout << "Result is: " << result << endl;
+        cout << "Do you want to continue? [Y/N]: ";
+        cin >> willContinue;
+        if (willContinue == "Y" || willContinue == "y") {
+            willContinueB = true;
+        }else if (willContinue == "N" || willContinue == "n") {
+            willContinueB = false;
+        }else {
+            cout << "Wrong input! the program will terminate" << endl;
+            break;
+        }
+    } while (willContinueB);
 
     return 0;
 }
