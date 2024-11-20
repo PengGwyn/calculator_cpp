dbb3e22da8ee1685781ab10a6dd557e4e21fe454
Gwynard C. Andaya <87253742+PengGwyn@users.noreply.github.com>
2024-11-20 10:35:10 +0800

    add files

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int option = 0;
+    float num1 = 0, num2 = 0, result = 0;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+    cout << "Choose operation: [1] addition, [2] subtraction, [3] multiplication, [4] division: ";
+    cin >> option;
+
+    if (option == 1) // addition
+    {
+        result = num1 + num2;
+    }
+    else if (option == 2)
+    {
+        result = num1 - num2;
+    }
+    else if (option == 3)
+    {
+        result = num1 * num2;
+    }
+    else if (option == 4)
+    {
+        result = num1 / num2;
+    }
+
+    cout << "Result is: " << result << endl;
+
+
+    return 0;
+}
